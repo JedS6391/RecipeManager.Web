8306c65f9c2e441099225a805d21965e0f4bc07d
Jed Simson <jed.simson@gmail.com>
2020-04-25 16:18:04 +1200

    Update production environment configuration.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  auth0Domain: '',
-  auth0ClientId: '',
-  auth0Audience: '',
-  baseUrl: ''
+  auth0Domain: 'dev-recipemanager.au.auth0.com',
+  auth0ClientId: 'RcS9g50SmWCktkcfAag0gfOF4iJXKB3i',
+  auth0Audience: 'https://jedsimson.co.nz/RecipeManager',
+  baseUrl: 'https://recipe-manager-api.azurewebsites.net'
 };
