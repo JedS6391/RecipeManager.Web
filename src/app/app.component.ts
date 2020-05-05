cfcd79d076418a0afb9eb1d8644b6014b59bcc33
Jed Simson <jed.simson@gmail.com>
2020-05-06 08:40:50 +1200

    Adjust theme colours to a darker palette.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { trigger, style, query, transition, group, animate } from '@angular/animations';
 import { Store, select } from '@ngrx/store';
 import * as Sentry from '@sentry/browser';
 
@@ -7,10 +9,32 @@ import { recipeErrorState } from './recipes/store/recipes-store.selectors';
 import { cartErrorState } from './cart/store/cart-store.selectors';
 import { APP_CONFIGURATION, AppConfiguration } from './configuration/app-configuration';
 
+export const routerTransition = trigger('routerTransition', [
+  transition('* <=> *', [
+    query(':enter, :leave', style({ position: 'fixed', width: '100%' })
+      , { optional: true }),
+    group([
+      query(':enter', [
+        query('.c', [
+          style({ opacity: 0 }),
+          animate('0.5s', style({ opacity: 1 }))
+        ])
+      ], { optional: true }),
+      query(':leave', [
+        query('.c', [
+          style({ opacity: 1 }),
+          animate('0.3s', style({ opacity: 0 }))
+        ])
+      ], { optional: true }),
+    ])
+  ])
+]);
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [routerTransition]
 })
 export class AppComponent implements OnInit {
   title = 'RecipeManager';
@@ -32,5 +56,9 @@ export class AppComponent implements OnInit {
     this.errorService.registerErrorStream('cart', this.store.pipe(
       select(cartErrorState)
     ));
+  }
+
+  public getState(outlet: RouterOutlet) {
+    return outlet.activatedRouteData.state;
   }
 }
