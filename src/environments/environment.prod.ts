9819e954cea60fdacbc0a3f81127445a593dbd20
Jed Simson <jed.simson@gmail.com>
2020-05-06 14:53:05 +1200

    Update production authentication service credentials.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  auth0Domain: 'dev-recipemanager.au.auth0.com',
-  auth0ClientId: 'RcS9g50SmWCktkcfAag0gfOF4iJXKB3i',
+  auth0Domain: 'prod-recipemanager.au.auth0.com',
+  auth0ClientId: '1hIhWC9HSFINA3n2BdP8fsrHssZ9qQ41',
   auth0Audience: 'https://jedsimson.co.nz/RecipeManager',
   sentryDsn: 'https://c90c4646da81439cb1996c844e9d6dd7@o386903.ingest.sentry.io/5221669',
   baseUrl: 'https://recipe-manager-api.azurewebsites.net'
