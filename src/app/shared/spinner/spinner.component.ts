c7b241e5ad739745d441418f9099a8a0b2fd2e9a
Jed Simson <jed.simson@gmail.com>
2020-05-06 13:56:46 +1200

    Update spinner theming.

--- src/app/shared/spinner/spinner.component.ts
+++ src/app/shared/spinner/spinner.component.ts
@@ -3,9 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
     selector: 'app-spinner',
     templateUrl: './spinner.component.html',
-    styleUrls: ['./spinner.component.css']
+    styleUrls: ['./spinner.component.scss']
 })
 export class SpinnerComponent {
-    @Input()
-    public colour = 'white';
 }
